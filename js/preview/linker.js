9a0f50b19dcb02b270ada49d2085bd3126913779
Nina Philippova <nina.philippova@gmail.com>
2017-01-20 21:57:32 +1300

    fixed linker.js

--- js/preview/linker.js
+++ js/preview/linker.js
@@ -1,47 +1,43 @@
-define(function() {
-    'use strict';
+function annotate(code, ast) {
+    var refs = { code: code, shift: 0 };
+    annotateRecursive(ast, refs, true);
+    return refs.code;
+}
 
-    return {
+function annotateRecursive(astNode, refs, top) {
+    if (astNode.type !== 'tag' && astNode.type !== 'root')
+        return;
+
+    if (astNode.type === 'tag') {
+        var attributeCount = astNode.attributes.length;
+        var position = attributeCount > 0
+                     ? astNode.attributes[attributeCount-1].end
+                     : astNode.nameEnd;
+
+        var offset = position.offset + refs.shift;
+        var newAttributes = '';
+        if (top)
+            newAttributes += ' xmlns:psvg="http://ashmind.github.io/pedanticsvg"';
+        newAttributes += ' psvg:id="' + astNode.id + '"';
+
+        refs.code = refs.code.substring(0, offset)
+                  + newAttributes
+                  + refs.code.substring(offset);
+        refs.shift += newAttributes.length;
+    }
+
+    var keepTop = astNode.type === 'root';
+    var children = astNode.children;
+    for (var i = 0; i < children.length; i++) {
+        annotateRecursive(children[i], refs, top && keepTop);
+    }
+}
+
+function findByAstNode($root, astNode) {
+    return $root.find('[psvg\\:id=' + astNode.id + ']');
+}
+
+export default {
         annotate: annotate,
         findByAstNode: findByAstNode
-    };
-
-    function annotate(code, ast) {
-        var refs = { code: code, shift: 0 };
-        annotateRecursive(ast, refs, true);
-        return refs.code;
-    }
-
-    function annotateRecursive(astNode, refs, top) {
-        if (astNode.type !== 'tag' && astNode.type !== 'root')
-            return;
-
-        if (astNode.type === 'tag') {
-            var attributeCount = astNode.attributes.length;
-            var position = attributeCount > 0
-                         ? astNode.attributes[attributeCount-1].end
-                         : astNode.nameEnd;
-
-            var offset = position.offset + refs.shift;
-            var newAttributes = '';
-            if (top)
-                newAttributes += ' xmlns:psvg="http://ashmind.github.io/pedanticsvg"';
-            newAttributes += ' psvg:id="' + astNode.id + '"';
-
-            refs.code = refs.code.substring(0, offset)
-                      + newAttributes
-                      + refs.code.substring(offset);
-            refs.shift += newAttributes.length;
-        }
-
-        var keepTop = astNode.type === 'root';
-        var children = astNode.children;
-        for (var i = 0; i < children.length; i++) {
-            annotateRecursive(children[i], refs, top && keepTop);
-        }
-    }
-
-    function findByAstNode($root, astNode) {
-        return $root.find('[psvg\\:id=' + astNode.id + ']');
-    }
-});
+};
