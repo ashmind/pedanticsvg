7b368fc451e6b2989703b10af920232e0b52c58d
Nina Philippova <nina.philippova@gmail.com>
2017-01-20 22:11:58 +1300

    fixed  open-on-drop.js

--- js/open-on-drop.js
+++ js/open-on-drop.js
@@ -1,32 +1,32 @@
 export default function($target) {
-        /*
-        if (!window.FileReader)
-            return;
+    /*
+    if (!window.FileReader)
+        return;
 
-        var dragOverClass = 'drag-over';
+    var dragOverClass = 'drag-over';
 
-        $(document).on('dragover', function(e) {
-            $target.addClass(dragOverClass);
-            e.preventDefault();
-        });
+    $(document).on('dragover', function(e) {
+        $target.addClass(dragOverClass);
+        e.preventDefault();
+    });
 
-        $(document).on('dragend', function(e) {
-            $target.removeClass(dragOverClass);
-            e.preventDefault();
-        });
+    $(document).on('dragend', function(e) {
+        $target.removeClass(dragOverClass);
+        e.preventDefault();
+    });
 
-        $(document).on('drop', function(e) {
-            $target.removeClass(dragOverClass);
-            handleDrop(e.originalEvent.dataTransfer.files[0]);
-            e.preventDefault();
-            e.stopPropagation();
-        });*/
+    $(document).on('drop', function(e) {
+        $target.removeClass(dragOverClass);
+        handleDrop(e.originalEvent.dataTransfer.files[0]);
+        e.preventDefault();
+        e.stopPropagation();
+    });*/
+}
+
+function handleDrop(file) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        console.log(e.target);
     };
-
-    function handleDrop(file) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            console.log(e.target);
-        };
-        reader.readAsText(file);
-    }
+    reader.readAsText(file);
+}
