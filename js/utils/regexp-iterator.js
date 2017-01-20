41f776e7c7bed2026a62910b7a67348f127c755d
Nina Philippova <nina.philippova@gmail.com>
2017-01-20 22:05:07 +1300

    fixed regxp-iterator.js

--- js/utils/regexp-iterator.js
+++ js/utils/regexp-iterator.js
@@ -1,58 +1,54 @@
-define(function() {
-    'use strict';
+export default function(string, regexp) {
+    var index = 0;
+    var nextResult;
 
-    return function(string, regexp) {
-        var index = 0;
-        var nextResult;
+    regexp = new RegExp('^(?:' + regexp.source + ')', regexp.ignoreCase ? 'i' : '');
+    return {
+        next: function() {
+            if (index >= string.length)
+                return { done: true };
 
-        regexp = new RegExp('^(?:' + regexp.source + ')', regexp.ignoreCase ? 'i' : '');
-        return {
-            next: function() {
-                if (index >= string.length)
-                    return { done: true };
-
-                var result;
-                if (nextResult) {
-                    result = nextResult;
-                    nextResult = null;
-                    return { value: result, done: false };
-                }
-
-                result = next(string, index, regexp);
-                index += result.string.length;
-                nextResult = result._next;
-                delete result._next;
+            var result;
+            if (nextResult) {
+                result = nextResult;
+                nextResult = null;
                 return { value: result, done: false };
             }
-        };
+
+            result = next(string, index, regexp);
+            index += result.string.length;
+            nextResult = result._next;
+            delete result._next;
+            return { value: result, done: false };
+        }
     };
+}
 
-    function next(string, index, regexp) {
-        var errorStart = 0;
-        var match = regexp.exec(string.substring(index));
-        if (!match) {
-            errorStart = index;
-            while (!match && index < string.length) {
-                index += 1;
-                match = regexp.exec(string.substring(index));
-            }
-
-            var result = {
-                string: string.substring(errorStart, index)
-            };
-            if (match) {
-                result._next = {
-                    string: match[0],
-                    match: match
-                };
-            }
-
-            return result;
+function next(string, index, regexp) {
+    var errorStart = 0;
+    var match = regexp.exec(string.substring(index));
+    if (!match) {
+        errorStart = index;
+        while (!match && index < string.length) {
+            index += 1;
+            match = regexp.exec(string.substring(index));
         }
 
-        return {
-            string: match[0],
-            match: match
+        var result = {
+            string: string.substring(errorStart, index)
         };
+        if (match) {
+            result._next = {
+                string: match[0],
+                match: match
+            };
+        }
+
+        return result;
     }
-});
+
+    return {
+        string: match[0],
+        match: match
+    };
+}
