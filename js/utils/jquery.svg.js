44e8625cf70acbae1cdb2b50468b2741ec79fbe1
Nina Philippova <nina.philippova@gmail.com>
2017-01-24 15:59:13 +1300

    jquery.svg.js var -> let

--- js/utils/jquery.svg.js
+++ js/utils/jquery.svg.js
@@ -2,14 +2,14 @@ import $ from 'jquery';
 
     $.extend($.fn, {
         svgAddClass: function(className) {
-            for (var i = 0; i < this.length; i++) {
+            for (let i = 0; i < this.length; i++) {
                 this[i].classList.add(className);
             }
             return this;
         },
 
         svgRemoveClass: function(className) {
-            for (var i = 0; i < this.length; i++) {
+            for (let i = 0; i < this.length; i++) {
                 this[i].classList.remove(className);
             }
             return this;
